31d66c0cabb4463f9f0aabc54ffd0470b7335e9d
Your Name <you@example.com>
2016-12-26 21:02:56 +0000

    sdf

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ln -s /root/go /usr/local/go
 
-echo "/root/go/src/github.com/mattermost/    *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-exportfs -a
 /usr/sbin/sshd
-echo "Starting fake server"
-cd /root/
-/usr/local/go/bin/go run http1.go
+#echo "Starting fake server"
+cd /root/go/
+#/usr/local/go/bin/go run http1.go
+git clone  https://github.com/mattermost/platform.git
+make run
